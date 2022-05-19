19db430154727c6051a104b31500c38ad8cda8d9
Ayush Sharma <ayush20892@gmail.com>
2022-05-20 02:59:23 +0530

    refactor: store updated

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,11 +1,9 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import userReducer from "../features/user/userSlice";
 import postReducer from "../features/post/postSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     user: userReducer,
     post: postReducer,
   },
